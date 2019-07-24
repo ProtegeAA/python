4770378c991b23daa53b810bae44a603888e6f79
Jeff <albert.anderson@gmail.com>
2019-07-23 19:32:21 -0700

    Update leap.py
    My first submission didn't pass the test suite! This one does.

--- leap.py
+++ leap.py
@@ -1,17 +1,12 @@
 def leap_year(year):
-    print("This is a leap year!")
-def leap_year_not(year):
-    print("This is not a leap year!")
-
-year = input("Enter a year: ")
-if (int(year) % 4) > 0:
-    leap_year_not(year)
-else:
-    if ((int(year) % 100) == 0) and ((int(year) % 400) == 0):
-        leap_year(year)
+   if (int(year) % 4) > 0:
+    return False
+   else:
+    if ((int(year) % 400) == 0):
+        return True
     else:
-        leap_year_not(year)
-        
-        
-            
-
+     if ((int(year) % 100) != 0):
+        return True
+     else:
+      if ((int(year) % 100) == 0):
+         return False          
